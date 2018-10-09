743d9445d367c77e96a408699d596f054dbf8945
siam@inflack <Shadman Ahmed>
2018-10-09 18:25:02 +0600

    WIP: RoomType - Hostel Information
    - Add RoomType repository and service
    - Add RoomTypeController
    - Implement RoomType store

---
+++ Modules/HM/Repositories/RoomTypeRepository.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: siaminflack
+ * Date: 10/9/18
+ * Time: 5:08 PM
+ */
+
+namespace Modules\HM\Repositories;
+
+
+use App\Repositories\AbstractBaseRepository;
+use Modules\HM\Entities\RoomType;
+
+class RoomTypeRepository extends AbstractBaseRepository
+{
+    protected $modelName = RoomType::class;
+}
