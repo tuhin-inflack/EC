428ad8df63477c81b68f6baf493ea3d82c60125e
siam@inflack <Shadman Ahmed>
2018-10-15 19:56:54 +0600

    WIP: theme layout

--- resources/views/layouts/partials/menu.blade.php
+++ resources/views/layouts/partials/menu.blade.php
@@ -5,10 +5,10 @@
                                                                                        data-i18n="nav.dash.main">Dashboard</span><span
                             class="badge badge badge-info badge-pill float-right mr-2">3</span></a>
                 <ul class="menu-content">
-                    <li><a class="menu-item" href="dashboard-ecommerce.html"
+                    <li class="active"><a class="menu-item" href="dashboard-ecommerce.html"
                            data-i18n="nav.dash.ecommerce">eCommerce</a>
                     </li>
-                    <li class="active"><a class="menu-item" href="dashboard-crypto.html" data-i18n="nav.dash.crypto">Crypto</a>
+                    <li class="menu-item"><a class="menu-item" href="dashboard-crypto.html" data-i18n="nav.dash.crypto">Crypto</a>
                     </li>
                     <li><a class="menu-item" href="dashboard-sales.html" data-i18n="nav.dash.sales">Sales</a>
                     </li>
