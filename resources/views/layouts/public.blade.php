3f471528a705b0cddb667fd2485bb78cdf8bac43
Md. Jahangir Alam <jahangir.alam@bs-23.com>
2018-10-16 15:20:50 +0600

    add a public layout

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -7,7 +7,7 @@
     <meta name="description"
           content="BARD ERP Solution.">
     <meta name="keywords"
-    content="BARD ERP Solution">
+          content="BARD ERP Solution">
     <meta name="author" content="Brain Station - 23 & Inflack Limited.">
 
     <!-- CSRF Token -->
@@ -41,54 +41,13 @@
 <!-- fixed-top-->
 @include('layouts.partials.fixed_top')
 <!-- ////////////////////////////////////////////////////////////////////////////-->
-{{--@include('layouts.partials.menu')--}}
-{{--<div class="app-content content">
+<div class="app-content content">
     <div class="content-wrapper">
         <div class="content-header row">
         </div>
         <div class="content-body">
-
+            @yield('content')
         </div>
-    </div>
-</div>--}}
-<div class="container">
-    <div id="crypto-stats-3" class="row" style="margin-top: 32px">
-            @foreach(array_keys(\Nwidart\Modules\Facades\Module::all()) as $module)
-                <div class="col-xl-4 col-12">
-                    <div class="card crypto-card-3 pull-up">
-                        <div class="card-content">
-                            <div class="card-body pb-0">
-                                <div class="row">
-                                    <div class="col-2">
-                                        <h1><i class="cc BTC warning font-large-2" title="BTC"></i></h1>
-                                    </div>
-                                    <div class="col-5 pl-2">
-                                        <h4>{{ $module }}</h4>
-                                        <h6 class="text-muted">{{ $module }}</h6>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-                <div class="col-xl-4 col-12">
-                    <div class="card crypto-card-3 pull-up">
-                        <div class="card-content">
-                            <div class="card-body pb-0">
-                                <div class="row">
-                                    <div class="col-2">
-                                        <h1><i class="cc BTC warning font-large-2" title="BTC"></i></h1>
-                                    </div>
-                                    <div class="col-5 pl-2">
-                                        <h4>Admin</h4>
-                                        <h6 class="text-muted"><a href="{{url('/user/role')}}">Administration</a></h6>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
     </div>
 </div>
 <!-- ////////////////////////////////////////////////////////////////////////////-->
