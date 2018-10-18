85de8bc3fd49d4a3f6bc971026dfb37a9585677a
Md Abu Taleb <abutaleb@bs-23.net>
2018-10-18 19:37:52 +0600

    employee education migration added

---
+++ Modules/HRM/Database/Migrations/2018_10_18_133400_create_employee_educations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeeEducationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee_educations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('institute_name')->nullable();
+            $table->string('degree_name')->nullable();
+            $table->string('department')->nullable();
+            $table->year('passing_year')->nullable();
+            $table->string('medium')->comment('Bengali, English etc')->nullable();
+            $table->string('duration')->nullable();
+            $table->string('result')->nullable();
+            $table->string('achievement')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee_educations');
+    }
+}
