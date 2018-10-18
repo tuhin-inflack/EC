31cc25c558e48fa412b3ad38441887b8ef5b9ff6
Md Abu Taleb <abutaleb@bs-23.net>
2018-10-18 19:20:41 +0600

    employee general info migration added

---
+++ Modules/HRM/Entities/EmployeeGeneralInfo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\HRM\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EmployeeGeneralInfo extends Model
+{
+	protected $table = "employee_general_info";
+    protected $fillable = [];
+}
