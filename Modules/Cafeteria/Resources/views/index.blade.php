492c3181e629dbbea57946a21d70fc16cc1d95d9
Md Abu Taleb <abutaleb@bs-23.net>
2018-10-18 16:44:27 +0600

    Cafeteria Module added

---
+++ Modules/Cafeteria/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('cafeteria::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('cafeteria.name') !!}
+    </p>
+@stop
