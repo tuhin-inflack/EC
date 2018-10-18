cb59bab19a01654cb79128a47153a2577d4225fb
Md Abu Taleb <abutaleb@bs-23.net>
2018-10-18 19:32:59 +0600

    employee personal info migration added

---
+++ Modules/HRM/Database/Migrations/2018_10_18_132514_create_employee_personal_infos_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeePersonalInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee_personal_info', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('father_name')->nullable();
+            $table->string('mother_name')->nullable();
+            $table->string('title')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->date('job_joining_date')->nullable();
+            $table->date('current_position_joining_date')->nullable();
+            $table->date('current_position_expire_date')->nullable();
+            $table->integer('salary_scale')->nullable();
+            $table->integer('total_salary')->nullable();
+            $table->string('marital_status')->nullable();
+            $table->integer('number_of_children')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee_personal_info');
+    }
+}
