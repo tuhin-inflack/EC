51f5d2909a4077d9ae128eede0e67b2a9c5b7b97
siam@inflack <Shadman Ahmed>
2018-10-21 16:20:24 +0600

    Implements: Hostel update feature

--- Modules/HM/Resources/views/hostel/edit.blade.php
+++ Modules/HM/Resources/views/hostel/edit.blade.php
@@ -7,7 +7,7 @@
                 <div class="card">
                     <div class="card-header">Update Hostel</div>
                     <div class="card-body">
-                        <form novalidate action="{{ route('hostels.update', $hostel->id) }}" method="post">
+                        <form action="{{ route('hostels.update', $hostel->id) }}" method="post">
                             @method('PUT')
                             @csrf
                             <div class="form-group row">
@@ -78,7 +78,7 @@
                                 <b class="col-sm-4 col-form-label text-md-right"><u>Room Details</u></b>
                             </div>
 
-                            <div class="repeater-default col-md-6 offset-sm-3 row">
+                            <div class="repeater-room-types col-md-6 offset-sm-3 row">
                                 <div class="mb-1 col-sm-12 col-md-4">
                                     <label for="bio" class="cursor-pointer">Room Type</label>
                                 </div>
@@ -97,7 +97,7 @@
                                     @foreach($hostel->roomTypes as $roomType)
                                         <div data-repeater-item class="row">
                                             <div class="form-group mb-1 col-sm-12 col-md-4">
-                                                <input type="hidden" name="id"
+                                                <input type="hidden" id="room-type-id" name="id"
                                                        value="{{ $roomType->id }}"/>
                                                 <input type="hidden" name="hostel_id" value="{{ $hostel->id }}"/>
                                                 <input type="text" name="name"
@@ -116,7 +116,7 @@
                                             </div>
                                             <div class="skin skin-flat form-group mb-1 col-sm-12 col-md-1">
                                                 <button type="button" class="btn btn-outline-danger"
-                                                        onclick="deleteRoomType(event)" data-repeater-delete>
+                                                        data-repeater-delete>
                                                     <i class="ft-x"></i>
                                                 </button>
                                             </div>
@@ -133,7 +133,7 @@
                                 </div>
                             </div>
                         </form>
-                        <form id="room-type-delete">
+                        <form id="room-type-delete" method="POST">
                             @csrf
                             @method('DELETE')
                         </form>
@@ -149,8 +149,46 @@
             type="text/javascript"></script>
     <script src="{{ asset('theme/js/scripts/forms/form-repeater.js') }}" type="text/javascript"></script>
     <script>
-        function deleteRoomType(e) {
-            
-        }
+        $(document).ready(function () {
+            $('.repeater-room-types').repeater({
+                show: function () {
+                    let index = $('div[data-repeater-item]').length;
+
+                    $('div[data-repeater-list="room_types"]').append(`
+                        <div data-repeater-item class="row">
+                            <div class="form-group mb-1 col-sm-12 col-md-4">
+                                <input type="hidden" id="room-type-id" name="room_types[${index}][id]" value>
+                                <input type="hidden" name="room_types[${index}][hostel_id]" value>
+                                <input type="text" name="room_types[${index}][name]" class="form-control">
+                            </div>
+                            <div class="form-group mb-1 col-sm-12 col-md-3">
+                                <input type="text" name="room_types[${index}][capacity]" class="form-control">
+                            </div>
+                            <div class="form-group mb-1 col-sm-12 col-md-3">
+                                <input type="text" name="room_types[${index}][rate]" class="form-control">
+                            </div>
+                            <div class="skin skin-flat form-group mb-1 col-sm-12 col-md-1">
+                                <button type="button" class="btn btn-outline-danger" data-repeater-delete="">
+                                    <i class="ft-x"></i>
+                                </button>
+                            </div>
+                        </div>
+                    `);
+                },
+                hide: function (deleteElement) {
+                    let roomTypeId = $(this).find('#room-type-id').val();
+
+                    if (roomTypeId) {
+                        if (confirm('Are you sure you want to delete this element?')) {
+                            $('#room-type-delete').attr('action', `/hm/room-types/${roomTypeId}`);
+                            $('#room-type-delete').submit();
+                            $(this).slideUp(deleteElement);
+                        }
+                    } else {
+                        $(this).slideUp(deleteElement);
+                    }
+                }
+            });
+        });
     </script>
 @endpush
