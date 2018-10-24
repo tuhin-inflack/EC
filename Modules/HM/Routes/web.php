c6cb21d728eabb3241c2028d612ce7bdb8f23631
siam@inflack <Shadman Ahmed>
2018-10-24 15:27:09 +0600

    Implements: Hostel room create form

--- Modules/HM/Routes/web.php
+++ Modules/HM/Routes/web.php
@@ -22,6 +22,10 @@ Route::prefix('hm')->group(function() {
         Route::get('{hostel}', 'HostelController@show')->name('hostels.show');
         Route::put('{hostel}', 'HostelController@update')->name('hostels.update');
         Route::delete('{hostel}', 'HostelController@destroy')->name('hostels.destroy');
+
+        // hostel rooms
+        Route::get('{hostel}/rooms/create', 'HostelRoomController@create')->name('hostel-rooms.create');
+
     });
 
     Route::prefix('room-types')->group(function () {
@@ -31,6 +35,10 @@ Route::prefix('hm')->group(function() {
         Route::get('{roomType}/edit', 'RoomTypeController@edit')->name('room-types.edit');
         Route::put('{roomType}', 'RoomTypeController@update')->name('room-types.update');
         Route::delete('{roomType}', 'RoomTypeController@destroy')->name('room-types.destroy');
+    });
+
+    Route::prefix('rooms')->group(function () {
+        Route::post('/', 'HostelRoomController@store')->name('rooms.store');
     });
 });
 
