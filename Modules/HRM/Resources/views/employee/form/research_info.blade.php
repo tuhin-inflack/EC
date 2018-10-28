fb7450e166e76943b7cec4538663017f6337d83a
Md Abu Taleb <abutaleb@bs-23.net>
2018-10-28 16:11:35 +0600

    all is well at my end

--- Modules/HRM/Resources/views/employee/form/research_info.blade.php
+++ Modules/HRM/Resources/views/employee/form/research_info.blade.php
@@ -1,37 +1,57 @@
-<div class="row">
-    <div class="col-md-6">
-        <div class="form-group">
-            {{ Form::label('organization_name', 'Organization Name') }}
-            {{ Form::text('organization_name', null, ['class' => 'form-control', 'placeholder' => 'eg. Microsoft']) }}
-        </div>
-    </div>
-    <div class="col-md-6">
-        <div class="form-group">
-            {{ Form::label('research_topic', 'Research Topic') }}
-            {{ Form::text('research_topic', null, ['class' => 'form-control', 'placeholder' => 'eg. Ethics']) }}
-        </div>
-    </div>
+<div class="repeater-default">
 
+    <div data-repeater-list="research">
+        <div data-repeater-item="">
+            <div class="row">
+                {{--<form class="form">--}}
+                <div class=" col-md-10">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                {{ Form::label('organization_name', 'Organization Name') }}
+                                {{ Form::text('organization_name', null, ['class' => 'form-control', 'placeholder' => 'eg. Microsoft']) }}
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                {{ Form::label('research_topic', 'Research Topic') }}
+                                {{ Form::text('research_topic', null, ['class' => 'form-control', 'placeholder' => 'eg. Ethics']) }}
+                            </div>
+                        </div>
+
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                {{ Form::label('responsibilities', 'Responsibilities') }}
+                                {{ Form::textarea('responsibilities', null, ['class' => 'form-control', 'placeholder' => '']) }}
+                            </div>
+                        </div>
+                        {{ Form::hidden('employee_id', $employee_id, ['class' =>'EmployeeId']) }}
+
+                        <hr>
+                    </div>
+                </div>
+                <div class=" col-md-2">
+                    <div class="form-group col-sm-12 col-md-2 mt-2">
+                        <button type="button" class="btn btn-danger" data-repeater-delete=""><i class="ft-x"></i>
+                            Remove
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <hr style="border-bottom: 1px solid #1E9FF2">
+        </div>
+    </div>
     <div class="col-md-12">
-        <div class="form-group">
-            {{ Form::label('responsibilities', 'Responsibilities') }}
-            {{ Form::textarea('responsibilities', null, ['class' => 'form-control', 'placeholder' => '']) }}
-        </div>
+        <button type="button" data-repeater-create="" class="btn btn-primary addMore"><i class="ft-plus"></i>
+            Add More
+        </button>
     </div>
-
-
-    <hr>
-
     <div class="form-actions col-md-12 ">
         <div class="pull-right">
-            {{ Form::button('<i class="la la-check-square-o"></i> Save', ['type' => 'submit', 'class' => 'btn btn-primary'] )  }}
+            {{ Form::button('<i class="la la-check-square-o"></i> Save', ['type' => 'submit', 'id' => 'SubmitButton', 'class' => 'btn btn-primary'] )  }}
             <a href="{{ url('/hrm/employee') }}">
-                <button type="button" class="btn btn-warning mr-1">
-                    <i class="la la-times"></i> Cancel
-                </button>
+                <button type="button" class="btn btn-warning mr-1"><i class="la la-times"></i> Cancel</button>
             </a>
-
         </div>
-
     </div>
 </div>
