0bb2a516074be8071d8996c5c25edf537706ac47
Md Abu Taleb <abutaleb@bs-23.net>
2018-10-28 16:11:35 +0600

    all is well at my end

--- Modules/HRM/Resources/views/employee/create.blade.php
+++ Modules/HRM/Resources/views/employee/create.blade.php
@@ -1,120 +1,27 @@
 @extends('hrm::layouts.master')
 @section('title', 'Add new employee ')
+@section("employee_create", 'active')
+
 
 @section("content")
-
     @php
         $tab_action = isset($employee_id) ? '' : 'disabled';
         $employee_id = isset($employee_id) ? $employee_id : '';
     @endphp
     <div class="col-xl-12 col-lg-12">
         <div class="card">
-<<<<<<< HEAD
-
-            @if(Session::has('message'))
-                <div class="alert alert-success text-center">
-                    {{Session::get('message')}}
-                </div>
-            @endif
-            <div class="card-header">
-                <h4 class="card-title">Add new employee </h4>
-            </div>
-            <div class="card-body">
-                <ul class="nav nav-tabs nav-underline nav-justified" id="tab-bottom-line-drag">
-                    <li class="nav-item">
-                        <a class="nav-link active" id="general-tab" data-toggle="tab" href="#general"
-                           aria-controls="activeIcon12" aria-expanded="true"><i class="la la-info"></i> General</a>
-                    </li>
-                    <li class="nav-item ">
-                        <a class="nav-link {{ $tab_action }} " id="personal-tab" data-toggle="tab" href="#personal"
-                           aria-controls="linkIcon12" aria-expanded="false"><i class="la la-archive"></i> Personal</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link {{ $tab_action }}" id="education-tab" data-toggle="tab" href="#education"
-                           aria-controls="linkIcon12"
-                           aria-expanded="false"><i class="la la-graduation-cap"></i> Education</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link {{ $tab_action }}" id="training-tab" data-toggle="tab" href="#training"
-                           aria-controls="linkIcon12"
-                           aria-expanded="false"><i class="la la-book"></i> Training</a>
-                    </li>
-
-
-                    <li class="nav-item">
-                        <a class="nav-link {{ $tab_action }}" id="publication-tab" data-toggle="tab" href="#publication"
-                           aria-controls="linkIcon12"
-                           aria-expanded="false"><i class="la la-paperclip"></i> Publication</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link {{ $tab_action }}" id="research-tab" data-toggle="tab" href="#research"
-                           aria-controls="linkIcon12"
-                           aria-expanded="false"><i class="la la-bookmark"></i> Research</a>
-                    </li>
-
-
-                </ul>
-                <div class="tab-content px-1 pt-1">
-
-
-                    <div role="tabpanel" class="tab-pane active show" id="general" aria-labelledby="general-tab"
-                         aria-expanded="true">
-                        {!! Form::open(['url' => 'hrm/employee/general-info', 'class'=>'form']) !!}
-                        @include('hrm::employee.form.general_info')
-                        {!! Form::close() !!}
-                    </div>
-
-
-                    <div class="tab-pane" id="personal" role="tabpanel" aria-labelledby="personal-tab"
-                         aria-expanded="false">
-                        {!! Form::open(['url' => 'hrm/employee/personal-info', 'class'=>'form']) !!}
-                        @include('hrm::employee.form.personal_info')
-                        {!! Form::close() !!}
-                    </div>
-
-                    <div class="tab-pane" id="education" role="tabpanel" aria-labelledby="education-tab"
-                         aria-expanded="false">
-                        {!! Form::open(['url' => 'hrm/employee/education_info', 'class'=>'form']) !!}
-                        @include('hrm::employee.form.education_info')
-                        {!! Form::close() !!}
-                    </div>
-
-
-                    <div class="tab-pane" id="training" role="tabpanel" aria-labelledby="training-tab"
-                         aria-expanded="false">
-                        {!! Form::open(['url' => 'hrm/employee/training-info', 'class'=>'form']) !!}
-                        @include('hrm::employee.form.training_info')
-                        {!! Form::close() !!}
-                    </div>
-=======
->>>>>>> 1637a374d4cd8b7cc7f8f82d272599c10a60054e
 
             {{--<div class="card-header">--}}
             {{--<h4 class="card-title">Add new employee </h4>--}}
             {{--</div>--}}
-            <div class="card-header">
-
-<<<<<<< HEAD
-                    <div class="tab-pane" id="publication" role="tabpanel" aria-labelledby="publication-tab"
-                         aria-expanded="false">
-                        {!! Form::open(['url' => 'hrm/employee/publication-info', 'class'=>'form']) !!}
-                        @include('hrm::employee.form.publication_info')
-                        {!! Form::close() !!}
-                    </div>
-
-
-                    <div class="tab-pane" id="research" role="tabpanel" aria-labelledby="research-tab"
-                         aria-expanded="false">
-                        {!! Form::open(['url' => 'hrm/employee/research-info', 'class'=>'form']) !!}
-                        @include('hrm::employee.form.research_info')
-                        {!! Form::close() !!}
-=======
+            <div class="col-md-10 col-md-offset-1">
                 @if(Session::has('message'))
                     <div class="alert alert-success text-center">
                         {{Session::get('message')}}
                     </div>
                 @endif
+            </div>
+            <div class="card-header">
                 <h4 class="card-title" id="repeat-form">Add New Employee</h4>
                 <a class="heading-elements-toggle"><i class="la la-ellipsis-h font-medium-3"></i></a>
                 <div class="heading-elements">
@@ -219,7 +126,6 @@
                             @include('hrm::employee.form.research_info')
                             {!! Form::close() !!}
                         </div>
->>>>>>> 1637a374d4cd8b7cc7f8f82d272599c10a60054e
                     </div>
                 </div>
             </div>
@@ -237,7 +143,7 @@
         console.log(employee_id);
         $(document).ready(function () {
             $('.addMore').click(function () {
-                $('.EmployeeId').val(employee_id) ;
+                $('.EmployeeId').val(employee_id);
             });
         })
     </script>
