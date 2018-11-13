2e338bbb96a5d666bf7d01579b3c83211580828d
Md Abu Taleb <abutaleb@bs-23.net>
2018-11-13 19:16:40 +0600

    employee research information update done

--- Modules/HRM/Resources/views/employee/view/research_details.blade.php
+++ Modules/HRM/Resources/views/employee/view/research_details.blade.php
@@ -4,18 +4,18 @@
 
         <tr>
             <th scope="col">Organization name</th>
-            <td>{{$research->organization_name}}</td>
+            <td>{{ $research->organization_name}}</td>
         </tr>
         <tr>
             <th scope="col">Research topic</th>
-            <td>{{$research->topic}}</td>
+            <td>{{ $research->research_topic}}</td>
         </tr>
         <tr>
             <th scope="col">Responsibilities</th>
-            <td>{{$research->responsibility}}</td>
+            <td>{{ $research->responsibilities}}</td>
         </tr>
 
         </tbody>
     </table>
 @endforeach
-<a class="btn btn-small btn-info" href="{{ url('/hrm/employee/' . $employee->id . '/edit#education') }}">Edit </a>
+<a class="btn btn-small btn-info" href="{{ url('/hrm/employee/' . $employee->id . '/edit#research') }}">Edit </a>
