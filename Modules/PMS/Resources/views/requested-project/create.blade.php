94a06b10c79219ac6ff181439050e31dc17f7b2f
saiduzzaman tuhin <tuhin@inflack.com>
2018-11-13 15:48:40 +0600

    project proposal submission form page html design

---
+++ Modules/PMS/Resources/views/requested-project/create.blade.php
@@ -0,0 +1,18 @@
+@extends('pms::layouts.master')
+@section('title', 'Add New Project Proposal Request ')
+
+@push('page-css')
+    <link rel="stylesheet" type="text/css" href="{{  asset('theme/vendors/css/pickers/pickadate/pickadate.css') }}">
+    <link rel="stylesheet" href="{{ asset('theme/vendors/css/pickers/daterange/daterangepicker.css')  }}">
+    <link rel="stylesheet" href="{{ asset('theme/css/plugins/pickers/daterange/daterange.css')  }}">
+@endpush
+
+@section('content')
+    @include('pms::requested-project.form')
+@endsection
+
+@push('page-js')
+    <script src="{{ asset('theme/vendors/js/pickers/pickadate/picker.js')  }}"></script>
+    <script src="{{ asset('theme/vendors/js/pickers/pickadate/picker.date.js') }}"></script>
+    <script src="{{ asset('theme/js/scripts/pickers/dateTime/pick-a-datetime.js')  }}"></script>
+@endpush
