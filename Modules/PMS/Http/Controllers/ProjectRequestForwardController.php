9c42ca5a339cfcb876314fcd7e099ef5917c294d
saiduzzaman tuhin <tuhin@inflack.com>
2018-11-13 12:13:23 +0600

    project proposal request form page html design

--- Modules/PMS/Http/Controllers/ProjectRequestForwardController.php
+++ Modules/PMS/Http/Controllers/ProjectRequestForwardController.php
@@ -39,7 +39,7 @@ class ProjectRequestForwardController extends Controller
      */
     public function create()
     {
-        return view('pms::create');
+        return view('pms::request-forward.create');
     }
 
     /**
--- Modules/PMS/Resources/views/project-request/form.blade.php
+++ Modules/PMS/Resources/views/project-request/form.blade.php
@@ -15,26 +15,32 @@
             </div>
             <div class="card-content collapse show">
                 <div class="card-body">
-                    {!! Form::open(['route' => 'project-request.store','enctype' => 'multipart/form-data','method' => 'POST']) !!}
+                    {!! Form::open() !!}
                         <div class="form-body">
                             <h4 class="form-section"><i class="la la-briefcase"></i> Project Proposal Request Form</h4>
                             <div class="row">
-                                <div class="col-md-6">
+                                <div class="col-md-12">
                                     <div class="form-group">
                                         <label for="name" class="form-label">Send To</label>
-                                        <input type="text"
-                                               value="{{ old('send_to') }}"
-                                               class="form-control{{ $errors->has('send_to') ? ' is-invalid' : '' }}"
-                                               name="send_to" autofocus required/>
-                                        @if ($errors->has('send_to'))
-                                            <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('send_to') }}</strong>
-                                        </span>
-                                        @endif
-                                    </div>
+                                        <div class="input-group">
+                                            <select class="select2 form-control{{ $errors->has('send_to') ? ' is-invalid' : '' }}" multiple="multiple">
+                                                <option value="AK">tuhin@inflack.com</option>
+                                                <option value="HI">sahib@inflack.com</option>
+                                                <option value="HI">siam@inflack.com</option>
+                                                <option value="HI">tanvir@inflack.com</option>
+                                                <option value="HI">jahangir@inflack.com</option>
+                                            </select>
 
+
+                                            @if ($errors->has('send_to'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('send_to') }}</strong>
+                                                </span>
+                                            @endif
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="col-md-6">
+                                <div class="col-md-12">
                                     <div class="form-group">
                                         <label for="name" class="form-label">Last Submission Date</label>
                                         <div class="input-group">
@@ -46,7 +52,7 @@
                                             <input type='text'
                                                    class="form-control pickadate-format-db {{ $errors->has('end_date') ? ' is-invalid' : '' }}"
                                                    placeholder="Pick a Date" name="end_date"
-                                                   required/>
+                                                   />
                                             @if ($errors->has('end_date'))
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $errors->first('end_date') }}</strong>
@@ -58,29 +64,10 @@
                                 </div>
                                 <div class="col-md-12">
                                     <div class="form-group">
-                                        <label for="name" class="form-label">Title</label>
-                                        <div class="input-group">
-
-                                            <input type="text"
-                                                   value="{{ old('title') }}"
-                                                   class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}"
-                                                   name="title" autofocus required/>
-                                            @if ($errors->has('title'))
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $errors->first('title') }}</strong>
-                                                </span>
-                                            @endif
-                                        </div>
-                                    </div>
-
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <label for="name" class="form-label">Message</label>
+                                        <label for="name" class="form-label">Proposal Request Remarks</label>
                                         <textarea name="message"
-                                                  class="form-control{{ $errors->has('message') ? ' is-invalid' : '' }}"
-                                                  id="" cols="30" rows="10"
-                                                  required>{{ old('message') }}</textarea>
+                                                  class="form-control{{ $errors->has('message') ? ' is-invalid' : '' }}" placeholder="Write here..."
+                                                  id="" cols="30" rows="5">{{ old('message') }}</textarea>
 
                                         @if ($errors->has('message'))
                                             <span class="invalid-feedback" role="alert">
@@ -90,27 +77,13 @@
                                     </div>
 
                                 </div>
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <label for="name" class="form-label">Attachment</label>
-                                        <input type="file"
-                                               class="form-control{{ $errors->has('attachment') ? ' is-invalid' : '' }}"
-                                               name="attachment" autofocus required/>
-                                        @if ($errors->has('attachment'))
-                                            <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('attachment') }}</strong>
-                                        </span>
-                                        @endif
-                                    </div>
-
-                                </div>
-                                <input type="hidden" name="status" value="0">
                             </div>
 
                             <div class="form-actions">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="la la-check-square-o"></i> Save
-                                </button>
+
+
+                                <a href="{{ route('project-request.index')  }}" class="btn btn-primary"><i class="la la-check-square-o"></i> Save</a>
+
                                 <a class="btn btn-warning mr-1" role="button" href="{{route('project-request.index')}}">
                                     <i class="ft-x"></i> Cancel
                                 </a>
