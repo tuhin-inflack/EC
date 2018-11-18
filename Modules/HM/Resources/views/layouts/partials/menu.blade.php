6854f85d701d9296db484266995972554c64debd
siam@inflack <Shadman Ahmed>
2018-11-18 16:08:56 +0600

    Implements: InventoryType, InventoryItem create form ui
    - Add InventoryTypeController and InventoryItemController
    - Add routes in hm/web.php
    - Update hm/menu.blade.php
    - Add bootstrap select2 css
    - Update app.blade.php

--- Modules/HM/Resources/views/layouts/partials/menu.blade.php
+++ Modules/HM/Resources/views/layouts/partials/menu.blade.php
@@ -30,11 +30,6 @@
                             <span class="menu-title"
                                   data-i18n="nav.dash.main">List</span></a>
                     </li>
-                    {{--<li class="{{ is_active_route('rooms.create') }}"><a href="{{ route('rooms.create') }}">
-                            <i class="la la-plus"></i>
-                            <span class="menu-title"
-                                  data-i18n="nav.dash.main">Create</span></a>
-                    </li>--}}
                 </ul>
             </li>
             <li class="nav-item">
@@ -43,12 +38,33 @@
                     <span class="menu-title"
                           data-i18n="nav.templates.main">Room Type</span></a>
                 <ul class="menu-content">
-                    {{--<li class="{{ is_active_route('rooms.index') }}"><a href="{{ route('rooms.index') }}">
-                        <i class="la la-list"></i>
-                            <span class="menu-title"
-                                  data-i18n="nav.dash.main">List</span></a>
-                    </li>--}}
                     <li class="{{ is_active_route('room-types.create') }}"><a href="{{ route('room-types.create') }}">
+                            <i class="la la-plus"></i>
+                            <span class="menu-title"
+                                  data-i18n="nav.dash.main">Create</span></a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="#">
+                    <i class="la la-book"></i>
+                    <span class="menu-title"
+                          data-i18n="nav.templates.main">Inventory Type</span></a>
+                <ul class="menu-content">
+                    <li class="{{ is_active_route('inventory-types.create') }}"><a href="{{ route('inventory-types.create') }}">
+                            <i class="la la-plus"></i>
+                            <span class="menu-title"
+                                  data-i18n="nav.dash.main">Create</span></a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="#">
+                    <i class="la la-book"></i>
+                    <span class="menu-title"
+                          data-i18n="nav.templates.main">Inventory Item</span></a>
+                <ul class="menu-content">
+                    <li class="{{ is_active_route('inventory-items.create') }}"><a href="{{ route('inventory-items.create') }}">
                             <i class="la la-plus"></i>
                             <span class="menu-title"
                                   data-i18n="nav.dash.main">Create</span></a>
