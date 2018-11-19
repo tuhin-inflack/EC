08510493b096cddbe233ecbe6db9963ef0a922d0
Md Abu Taleb <abutaleb@bs-23.net>
2018-11-19 19:42:59 +0600

    department menu added

--- Modules/HRM/Resources/views/employee/view/personal_info_details.blade.php
+++ Modules/HRM/Resources/views/employee/view/personal_info_details.blade.php
@@ -2,48 +2,48 @@
     <tbody>
 
     <tr>
-        <th scope="col">Father's Name</th>
+        <th >Father's Name</th>
         <td>{{ $employee->employeePersonalInfo->father_name }}</td>
     </tr>
     <tr>
 
-        <th scope="col">Mother's Name</th>
+        <th >Mother's Name</th>
         <td>{{$employee->employeePersonalInfo->mother_name}}</td>
     </tr>
     <tr>
-        <th scope="col">Title Name</th>
+        <th >Title Name</th>
         <td>{{$employee->employeePersonalInfo->title}}</td>
     </tr>
     <tr>
-        <th scope="col">Date of Birth</th>
+        <th >Date of Birth</th>
         <td>{{$employee->employeePersonalInfo->date_of_birth}}</td>
     </tr>
     <tr>
-        <th scope="col">Job joining date</th>
+        <th >Job joining date</th>
         <td>{{$employee->employeePersonalInfo->job_joining_date}}</td>
     </tr>
     <tr>
-        <th scope="col">Current position joining date</th>
+        <th >Current position joining date</th>
         <td>{{$employee->employeePersonalInfo->current_position_joining_date }}</td>
     </tr>
     <tr>
-        <th scope="col">Current position expire date</th>
+        <th >Current position expire date</th>
         <td>{{$employee->employeePersonalInfo->current_position_expire_date}}</td>
     </tr>
     <tr>
-        <th scope="col">Salary scale</th>
+        <th >Salary scale</th>
         <td>{{$employee->employeePersonalInfo->salary_scale}}</td>
     </tr>
     <tr>
-        <th scope="col">Total salary</th>
+        <th >Total salary</th>
         <td>{{$employee->employeePersonalInfo->total_salary}}</td>
     </tr>
     <tr>
-        <th scope="col">Marital status</th>
+        <th >Marital status</th>
         <td>{{$employee->employeePersonalInfo->marital_status}}</td>
     </tr>
     <tr>
-        <th scope="col">Number of children</th>
+        <th >Number of children</th>
         <td>{{$employee->employeePersonalInfo->number_of_children}}</td>
     </tr>
 
