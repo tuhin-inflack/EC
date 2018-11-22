8e691ce03c0fcccef90537d844addd52a858c1df
Md Abu Taleb <abutaleb@bs-23.net>
2018-11-22 13:04:16 +0600

    department list done with excel, pdf, print & copy to clicpboard facilities

--- Modules/HRM/Http/Controllers/DepartmentController.php
+++ Modules/HRM/Http/Controllers/DepartmentController.php
@@ -17,24 +17,17 @@ class DepartmentController extends Controller {
 	}
 
 	public function index() {
-		return view( 'hrm::index' );
+		$departmentList = $this->departmentService->getDepartmentList();
+
+		return view( 'hrm::department.index', compact( 'departmentList' ) );
 	}
 
-	/**
-	 * Show the form for creating a new resource.
-	 * @return Response
-	 */
+
 	public function create() {
 		return view( 'hrm::department.create' );
 	}
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param  Request $request
-	 *
-	 * @return Response
-	 */
+
 	public function store( Request $request ) {
 		$response = $this->departmentService->storeDepartment( $request->all() );
 		Session::flash( 'message', $response->getContent() );
@@ -43,36 +36,20 @@ class DepartmentController extends Controller {
 
 	}
 
-	/**
-	 * Show the specified resource.
-	 * @return Response
-	 */
+
 	public function show() {
 		return view( 'hrm::show' );
 	}
 
-	/**
-	 * Show the form for editing the specified resource.
-	 * @return Response
-	 */
+
 	public function edit() {
 		return view( 'hrm::edit' );
 	}
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  Request $request
-	 *
-	 * @return Response
-	 */
+
 	public function update( Request $request ) {
 	}
 
-	/**
-	 * Remove the specified resource from storage.
-	 * @return Response
-	 */
 	public function destroy() {
 	}
 }
