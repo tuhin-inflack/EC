0896244e2c8c9b0535c844efa3694e2d4e732327
Md. Jahangir Alam <jahangir.alam@bs-23.com>
2018-11-25 11:29:19 +0600

    add user controller and service

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jahangir
+ * Date: 11/22/18
+ * Time: 1:09 PM
+ */
+
+namespace App\Http\Controllers;
+
+
+class UserController extends Controller
+{
+
+}
