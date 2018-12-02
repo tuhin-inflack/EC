4d07c26c85285aab793b3dc168973b222de55e1a
Md Abu Taleb <abutaleb@bs-23.net>
2018-12-02 12:27:15 +0600

    education dropdown at employee add

--- Modules/HRM/Services/AcademicInstituteService.php
+++ Modules/HRM/Services/AcademicInstituteService.php
@@ -32,7 +32,23 @@ class AcademicInstituteService {
 
 	public function storeInstitute( $data ) {
 		$institute = $this->save( $data );
+
 		return $institute;
+	}
+
+	public function getDegreeDuration() {
+		return [
+			'1 year'   => '1 year',
+			'2 years'  => '2 years',
+			'3 years'  => '3 years',
+			'4 years'  => '4 years',
+			'5 years'  => '5 years',
+			'6 years'  => '6 years',
+			'7 years'  => '7 years',
+			'8 years'  => '8 years',
+			'9 years'  => '9 years',
+			'10 years' => '10 years',
+		];
 	}
 
 }
