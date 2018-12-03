80fc681f3c928d4dbcc4d83f258e6a11f4a989a8
Md. Jahangir Alam <jahangir.alam@bs-23.com>
2018-12-03 20:01:30 +0600

    FBR_AUTH:006 Force password change

--- resources/views/layouts/partials/fixed_top.blade.php
+++ resources/views/layouts/partials/fixed_top.blade.php
@@ -221,6 +221,7 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{url('/change/password')}}">Change Password</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
