6ca3b5e6fa37ad116285d6039da221aa600b80a3
Md Abu Taleb <abutaleb@bs-23.net>
2018-12-05 17:12:43 +0600

    hostel budget section create done

--- Modules/HM/Http/Controllers/HostelBudgetSectionController.php
+++ Modules/HM/Http/Controllers/HostelBudgetSectionController.php
@@ -5,65 +5,67 @@ namespace Modules\HM\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Session;
 use Modules\HM\Http\Requests\StoreHostelBudgetSectionRequest;
+use Modules\HM\Services\HostelBudgetSectionService;
 
-class HostelBudgetSectionController extends Controller
-{
+class HostelBudgetSectionController extends Controller {
+	private $hostelBudgetSectionService;
 
-	public function __construct() {
+	public function __construct( HostelBudgetSectionService $hostelBudgetSectionService ) {
+		$this->hostelBudgetSectionService = $hostelBudgetSectionService;
 	}
 
-	public function index()
-    {
-    	$sections = [];
+	public function index() {
+		$sections = [];
 
-        return view('hm::hostel-budget-section.index', compact('sections'));
-    }
-
-
-    public function create()
-    {
-        return view('hm::hostel-budget-section.create');
-    }
+		return view( 'hm::hostel-budget-section.index', compact( 'sections' ) );
+	}
 
 
-    public function store(StoreHostelBudgetSectionRequest $request)
-    {
-    	dd($request->all());
-    }
+	public function create() {
+		return view( 'hm::hostel-budget-section.create' );
+	}
 
-    /**
-     * Show the specified resource.
-     * @return Response
-     */
-    public function show()
-    {
-        return view('hm::show');
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     * @return Response
-     */
-    public function edit()
-    {
-        return view('hm::edit');
-    }
+	public function store( StoreHostelBudgetSectionRequest $request ) {
+		$section = $this->hostelBudgetSectionService->storeHostelBudgetSection( $request->all() );
+		Session::flash( 'message', $section->getContent() );
 
-    /**
-     * Update the specified resource in storage.
-     * @param  Request $request
-     * @return Response
-     */
-    public function update(Request $request)
-    {
-    }
+		return redirect( '/hm/hostel-budget-section/' );
 
-    /**
-     * Remove the specified resource from storage.
-     * @return Response
-     */
-    public function destroy()
-    {
-    }
+	}
+
+	/**
+	 * Show the specified resource.
+	 * @return Response
+	 */
+	public function show() {
+		return view( 'hm::show' );
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 * @return Response
+	 */
+	public function edit() {
+		return view( 'hm::edit' );
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  Request $request
+	 *
+	 * @return Response
+	 */
+	public function update( Request $request ) {
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 * @return Response
+	 */
+	public function destroy() {
+	}
 }
