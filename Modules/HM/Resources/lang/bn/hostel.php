2cf66c86ea7828303c8d47780f100d7b4011c0c6
Md. Jahangir Alam <jahangir.alam@bs-23.com>
2018-12-09 11:48:34 +0600

    BE-30 Add a hostel -- **Hostel list page **Create hostel only without associated rooms

---
+++ Modules/HM/Resources/lang/bn/hostel.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'menu_title' => 'হোস্টেল',
+    'card_title' => 'হোস্টেল তালিকা',
+    'create_button' => 'নতুন হোস্টেল',
+    'create_card_title' => 'হোস্টেল তৈরি',
+    'create_form_title' => 'Hostel Form',
+    'edit_card_title' => 'হোস্টেল সম্পাদনা করুন',
+    'edit_form_title' => 'হোস্টেল সম্পাদনা ফর্ম',
+    'total_floor' => 'মোট মেঝে'
+];
