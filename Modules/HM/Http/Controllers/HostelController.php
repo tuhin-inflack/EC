1d865445cc96e1ea2f3b0cbe47ec18e3e1945157
Md. Jahangir Alam <jahangir.alam@bs-23.com>
2018-12-09 11:48:34 +0600

    BE-30 Add a hostel -- **Hostel list page **Create hostel only without associated rooms

--- Modules/HM/Http/Controllers/HostelController.php
+++ Modules/HM/Http/Controllers/HostelController.php
@@ -15,14 +15,16 @@ use Modules\HM\Services\RoomTypeService;
 class HostelController extends Controller
 {
     private $hostelService;
+    private $roomTypeService;
 
     /**
      * HostelController constructor.
      * @param HostelService $hostelService
      */
-    public function __construct(HostelService $hostelService)
+    public function __construct(HostelService $hostelService, RoomTypeService $roomTypeService)
     {
         $this->hostelService = $hostelService;
+        $this->roomTypeService = $roomTypeService;
     }
 
     /**
@@ -42,7 +44,8 @@ class HostelController extends Controller
      */
     public function create()
     {
-        return view('hm::hostel.create');
+        $roomTypes = $this->roomTypeService->pluck();
+        return view('hm::hostel.create', compact('roomTypes'));
     }
 
     /**
