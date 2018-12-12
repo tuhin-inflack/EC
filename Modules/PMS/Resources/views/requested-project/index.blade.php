eb686aa35313ae2002b14d0a5e7e146f6bf43e85
tuhindewan <tuhinsshadow@gmail.com>
2018-12-12 20:14:11 +0600

    Project proposal request list (individual) showing

--- Modules/PMS/Resources/views/requested-project/index.blade.php
+++ Modules/PMS/Resources/views/requested-project/index.blade.php
@@ -21,20 +21,26 @@
                                         <th scope="col">{{trans('labels.serial')}}</th>
                                         <th scope="col">{{trans('pms::project_proposal.received_at')}}</th>
                                         <th scope="col">{{trans('pms::project_proposal.remarks')}}</th>
+                                        <th scope="col">{{trans('pms::project_proposal.attached_file')}}</th>
                                         <th scope="col">{{trans('pms::project_proposal.last_sub_date')}}</th>
                                         <th scope="col">{{trans('labels.status')}}</th>
                                         <th scope="col">{{trans('labels.action')}}</th>
                                     </tr>
                                     </thead>
                                     <tbody>
-
+                                    @foreach($requests as $request)
                                     <tr>
-                                        <th scope="row">1</th>
-                                        <td>2018-11-08 16:15:12</td>
-                                        <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
-                                        <td>08/11/2018</td>
+                                        <th scope="row">{{ $loop->iteration }}</th>
+                                        <td>{{$request->created_at}}</td>
+                                        <td>{{ $request->message }}</td>
+                                        <td><a href="{{url('pms/project-requests/attachment-download/'.$request->id)}}">Attachment</a></td>
+                                        <td>{{ $request->end_date }}</td>
                                         <td>
-                                            <span class="badge badge-warning">Ongoing</span>
+                                            @if($request->status == 0)
+                                                <span class="badge badge-warning">Ongoing</span>
+                                            @else
+                                                <span class="badge badge-warning">Success</span>
+                                            @endif
                                         </td>
                                         <td>
                                             <span class="dropdown">
@@ -50,29 +56,7 @@
                                             </span>
                                         </td>
                                     </tr>
-                                    <tr>
-                                        <th scope="row">2</th>
-                                        <td>2018-11-08 16:15:12</td>
-                                        <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
-                                        <td>26/07/2019</td>
-                                        <td>
-                                            <span class="badge badge-success">Completed</span>
-                                        </td>
-                                        <td>
-                                            <span class="dropdown">
-                                                <button id="btnSearchDrop2" type="button" data-toggle="dropdown"
-                                                        aria-haspopup="true"
-                                                        aria-expanded="false" class="btn btn-info dropdown-toggle"><i
-                                                            class="la la-cog"></i></button>
-                                                <span aria-labelledby="btnSearchDrop2"
-                                                      class="dropdown-menu mt-1 dropdown-menu-right">
-                                                <a href="{{route('project-proposal-submission.create')}}"
-                                                   class="dropdown-item"><i class="ft-fast-forward"></i> Submit Proposal</a>
-                                                </span>
-                                            </span>
-                                        </td>
-                                    </tr>
-
+                                        @endforeach
                                     </tbody>
                                 </table>
                             </div>
