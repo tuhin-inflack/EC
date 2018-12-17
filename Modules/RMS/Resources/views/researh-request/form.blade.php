1a4468b3b423d4a38953e2b20eb148a78ddfb543
tuhindewan <tuhinsshadow@gmail.com>
2018-12-17 13:51:09 +0600

    Research proposal request form localization WIP

--- Modules/RMS/Resources/views/researh-request/form.blade.php
+++ Modules/RMS/Resources/views/researh-request/form.blade.php
@@ -1,8 +1,8 @@
 <div class="row match-height">
-<div class="col-md-8 offset-2">
+<div class="col-md-12">
         <div class="card">
             <div class="card-header">
-                <h4 class="card-title" id="basic-layout-form">Create Request Research Proposal</h4>
+                <h4 class="card-title" id="basic-layout-form">{{trans('rms::research_proposal.research_proposal_request_creation')}}</h4>
                 <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
                 <div class="heading-elements">
                     <ul class="list-inline mb-0">
@@ -17,92 +17,93 @@
                 <div class="card-body">
                     {!! Form::open() !!}
                     <div class="form-body">
-                        <h4 class="form-section"><i class="la la-briefcase"></i> Research Proposal Request Form</h4>
+                        <h4 class="form-section"><i class="la la-briefcase"></i> {{trans('rms::research_proposal.request_form')}}</h4>
                         <div class="row">
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label for="name" class="form-label">Departments</label>
-                                    <div class="input-group">
-                                        <select class="select2 form-control{{ $errors->has('send_to') ? ' is-invalid' : '' }}" multiple="multiple">
-                                            <option value="AK">Administration</option>
-                                            <option value="HI">Training</option>
-                                            <option value="HI">Research</option>
-                                            <option value="HI"></option>
-                                        </select>
+                            <div class="col-md-8 offset-2">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="name" class="form-label">Departments</label>
+                                            <div class="input-group">
+                                                <select class="select2 form-control{{ $errors->has('send_to') ? ' is-invalid' : '' }}" multiple="multiple">
+                                                    <option value="AK">Administration</option>
+                                                    <option value="HI">Training</option>
+                                                    <option value="HI">Research</option>
+                                                    <option value="HI"></option>
+                                                </select>
 
 
-                                        @if ($errors->has('send_to'))
-                                            <span class="invalid-feedback" role="alert">
+                                                @if ($errors->has('send_to'))
+                                                    <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $errors->first('send_to') }}</strong>
                                                 </span>
-                                        @endif
+                                                @endif
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label for="name" class="form-label">Start Date</label>
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="name" class="form-label">Start Date</label>
+                                            <div class="input-group">
+                                                <div class="input-group-prepend">
                                                 <span class="input-group-text">
                                                   <span class="la la-calendar-o"></span>
                                                 </span>
-                                        </div>
-                                        <input type='text'
-                                               class="form-control pickadate-format-db {{ $errors->has('end_date') ? ' is-invalid' : '' }}"
-                                               placeholder="Pick a Date" name="end_date"
-                                        />
-                                        @if ($errors->has('end_date'))
-                                            <span class="invalid-feedback" role="alert">
+                                                </div>
+                                                <input type='text'
+                                                       class="form-control pickadate-format-db {{ $errors->has('end_date') ? ' is-invalid' : '' }}"
+                                                       placeholder="Pick a Date" name="end_date"
+                                                />
+                                                @if ($errors->has('end_date'))
+                                                    <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $errors->first('end_date') }}</strong>
                                                 </span>
-                                        @endif
-                                    </div>
-                                </div>
+                                                @endif
+                                            </div>
+                                        </div>
 
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label for="name" class="form-label">End Date</label>
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="name" class="form-label">End Date</label>
+                                            <div class="input-group">
+                                                <div class="input-group-prepend">
                                                 <span class="input-group-text">
                                                   <span class="la la-calendar-o"></span>
                                                 </span>
-                                        </div>
-                                        <input type='text'
-                                               class="form-control pickadate-format-db {{ $errors->has('end_date') ? ' is-invalid' : '' }}"
-                                               placeholder="Pick a Date" name="end_date"
-                                        />
-                                        @if ($errors->has('end_date'))
-                                            <span class="invalid-feedback" role="alert">
+                                                </div>
+                                                <input type='text'
+                                                       class="form-control pickadate-format-db {{ $errors->has('end_date') ? ' is-invalid' : '' }}"
+                                                       placeholder="Pick a Date" name="end_date"
+                                                />
+                                                @if ($errors->has('end_date'))
+                                                    <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $errors->first('end_date') }}</strong>
                                                 </span>
-                                        @endif
+                                                @endif
+                                            </div>
+                                        </div>
+
                                     </div>
-                                </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="name" class="form-label">Notice</label>
+                                            <textarea name="message"
+                                                      class="form-control{{ $errors->has('message') ? ' is-invalid' : '' }}" placeholder="Write here..."
+                                                      id="" cols="30" rows="5">{{ old('message') }}</textarea>
 
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label for="name" class="form-label">Notice</label>
-                                    <textarea name="message"
-                                              class="form-control{{ $errors->has('message') ? ' is-invalid' : '' }}" placeholder="Write here..."
-                                              id="" cols="30" rows="5">{{ old('message') }}</textarea>
-
-                                    @if ($errors->has('message'))
-                                        <span class="invalid-feedback" role="alert">
+                                            @if ($errors->has('message'))
+                                                <span class="invalid-feedback" role="alert">
                                             <strong>{{ $errors->first('message') }}</strong>
                                         </span>
-                                    @endif
-                                </div>
+                                            @endif
+                                        </div>
 
+                                    </div>
+                                </div>
                             </div>
                         </div>
-
-                        <div class="form-actions">
-
-
+                        <div class="form-actions text-center">
                             <a href="{{ route('research-request.index')  }}" class="btn btn-primary"><i class="la la-check-square-o"></i> Save</a>
 
                             <a class="btn btn-warning mr-1" role="button" href="{{route('research-request.index')}}">
