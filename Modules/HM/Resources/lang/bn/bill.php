e4bffc1d93f2adc25bf3ab526e38c3c4c3c8ce11
tanvirinflack <tanvir@inflack.com>
2018-12-25 19:11:59 +0600

    ** User Seed modified
    - added only tanvir as admin user
    - default password: 123123

---
+++ Modules/HM/Resources/lang/bn/bill.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tanvirinflack
+ * Date: 12/24/18
+ * Time: 4:30 PM
+ */
+return [
+    'title' => 'Bill',
+    'list' => 'Bill List',
+    'bill_generate' => 'Generate Bill',
+    'bill_payment' => 'Bill Payment',
+    'bill_payment_list' => 'Payment Register',
+];
---
+++ Modules/HM/Resources/lang/en/bill.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shomrat
+ * Date: 12/24/18
+ * Time: 4:30 PM
+ */
