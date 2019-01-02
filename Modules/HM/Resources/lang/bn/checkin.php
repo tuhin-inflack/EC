38b5b5d505e47ba451ba563b4ac97e81feebb3cb
siam@inflack <Shadman Ahmed>
2019-01-02 18:01:15 +0600

    WIP: BE-163 Checkin list
    - Add checkin list localization

---
+++ Modules/HM/Resources/lang/bn/checkin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: siaminflack
+ * Date: 1/2/19
+ * Time: 5:50 PM
+ */
+
+return [
+    'check_in_number' => 'সংখ্যা চেক করুন',
+    'booking_id' => 'বুকিং আইডি',
+    'estimated_check_out_time' => 'আনুমানিক চেক আউট সময়',
+    'estimated_no_of_day' => 'আনুমানিক # দিন',
+];
