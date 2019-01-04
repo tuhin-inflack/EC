48baa144e751328fcaa67b95ebf97821f26271b5
tanvirinflack <tanvir@inflack.com>
2019-01-04 22:08:06 +0600

    UserSeeder modifyed for only two users

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,33 +14,6 @@ class UsersTableSeeder extends Seeder
     {
         $users = [
             (object)[
-                'name' => 'Tanvir Hossain',
-                'email' => 'tanvir@inflack.com',
-                'username' => 'tanvir',
-                'password' => '$2y$10$Hy3h5XfdQK2e3cgke7ebHevS4E7no2Z6149YDVKS5t7WJ7Y9pJyrS', // 123123
-                'user_type' => 'Admin',
-                'mobile' => '01710000000',
-                'last_password_change' => date('Y-m-d H:i:s')
-            ],
-            (object)[
-                'name' => 'BARD',
-                'email' => 'bard@erp.com',
-                'username' => 'bard',
-                'password' => '$2y$10$BTrCiO0lmLD94Nwsj23l5eedjtnDOKFBNaOJoMRirjIJLkqMf/YR2', // bard789
-                'user_type' => 'Guest',
-                'mobile' => '01710000000',
-                'last_password_change' => date('Y-m-d H:i:s')
-            ],
-            (object)[
-                'name' => 'Hasib',
-                'email' => 'hasib@brainstation-23.com',
-                'username' => 'hasib',
-                'password' => '$2y$10$fpl4aKLzKDhCm9DCNHWcMebhsYr9JEoRefb8Eot2l/ddSASRtd6gK',
-                'user_type' => 'Admin',
-                'mobile' => '01719117792',
-                'last_password_change' => date('Y-m-d H:i:s')
-            ],
-            (object)[
                 'name' => 'Director Admin',
                 'email' => 'dg@abc.com',
                 'username' => 'directoradmin',
