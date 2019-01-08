471e7aab4b53d5c2082b909bf4ce8a40b21fc03e
Md. Abutaleb <abutaleb@bs-23.net>
2019-01-08 11:04:41 +0600

    jquery datatables css added to theme

--- Modules/HRM/Resources/views/designation/index.blade.php
+++ Modules/HRM/Resources/views/designation/index.blade.php
@@ -82,13 +82,6 @@
 @endsection
 
 @push('page-js')
-    <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.flash.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js"></script>
-
 
     <script>
 
@@ -134,12 +127,5 @@
                 event.preventDefault();
         }
     </script>
-
-@endpush
-
-@push('page-css')
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css"/>
-    <link rel="stylesheet" type="text/css"
-          href="https://cdn.datatables.net/buttons/1.5.2/css/buttons.dataTables.min.css"/>
 
 @endpush
