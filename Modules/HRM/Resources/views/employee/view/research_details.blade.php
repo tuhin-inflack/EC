8876affd15408b552348d521f7ca4de10c442ad6
Md. Abutaleb <abutaleb@bs-23.net>
2019-01-13 12:12:16 +0600

    Research information localization done

--- Modules/HRM/Resources/views/employee/view/research_details.blade.php
+++ Modules/HRM/Resources/views/employee/view/research_details.blade.php
@@ -3,19 +3,19 @@
         <tbody>
 
         <tr>
-            <th scope="col">Organization name</th>
+            <th scope="col">@lang('hrm::research.organization_name')</th>
             <td>{{ $research->organization_name}}</td>
         </tr>
         <tr>
-            <th scope="col">Research topic</th>
+            <th scope="col">@lang('hrm::research.research_topic')</th>
             <td>{{ $research->research_topic}}</td>
         </tr>
         <tr>
-            <th scope="col">Responsibilities</th>
+            <th scope="col">@lang('hrm::research.responsibilities')</th>
             <td>{{ $research->responsibilities}}</td>
         </tr>
 
         </tbody>
     </table>
 @endforeach
-<a class="btn btn-small btn-info" href="{{ url('/hrm/employee/' . $employee->id . '/edit#research') }}">Edit </a>
+<a class="btn btn-small btn-info" href="{{ url('/hrm/employee/' . $employee->id . '/edit#research') }}">@lang('labels.edit') </a>
