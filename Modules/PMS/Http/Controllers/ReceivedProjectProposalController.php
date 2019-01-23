bcd21c0de8f415a2f2dd0d76c6e0e3c3bd4081f2
Md. Abutaleb <abutaleb@bs-23.net>
2019-01-23 12:36:28 +0600

    refactoring done for project research organization and member

--- Modules/PMS/Http/Controllers/ReceivedProjectProposalController.php
+++ Modules/PMS/Http/Controllers/ReceivedProjectProposalController.php
@@ -3,6 +3,7 @@
 namespace Modules\PMS\Http\Controllers;
 
 
+use App\Services\OrganizableService;
 use App\Services\OrganizationService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -10,18 +11,25 @@ use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Session;
 use Modules\PMS\Http\Requests\StoreOrganization;
+use Modules\PMS\Http\Requests\StoreOrganizationRequest;
 use Modules\PMS\Services\ProjectProposalService;
+use Prophecy\Doubler\Generator\TypeHintReference;
 
 
 class ReceivedProjectProposalController extends Controller
 {
     private $projectProposalService;
     private $organizationService;
+    private $organizableService;
 
-    public function __construct(ProjectProposalService $projectProposalService, OrganizationService $organizationService)
+
+    public function __construct(ProjectProposalService $projectProposalService,
+                                OrganizationService $organizationService,
+                                OrganizableService $organizableService)
     {
         $this->projectProposalService = $projectProposalService;
         $this->organizationService = $organizationService;
+        $this->organizableService = $organizableService;
     }
 
     public function index()
@@ -46,10 +54,29 @@ class ReceivedProjectProposalController extends Controller
     }
 
 
-
     public function edit()
     {
         return view('pms::edit');
+    }
+
+    public function addOrganization($projectId)
+    {
+
+        $type = Config::get('constants.project');
+
+        $organizations = $this->organizationService->getAllOrganization($projectId, $type);
+        $proposal = $this->projectProposalService->getProposalById($projectId);
+
+        return view('pms::proposal-submitted.add_organization', compact('proposal', 'organizations', 'type'));
+
+    }
+
+    public function storeOrganization(StoreOrganizationRequest $request, $projectId)
+    {
+        $response = $this->organizableService->storeData($request->all(), $projectId);
+        Session::flash('message', $response->getContent());
+
+        return redirect()->route('project-proposal-submitted.index');
     }
 
 
