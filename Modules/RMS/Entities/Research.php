ed0328aeca679bd21760e446b924b660c1beda1e
siam@inflack <Shadman Ahmed>
2019-01-28 12:45:08 +0600

    BE-402 Show organization list in research/project detail

--- Modules/RMS/Entities/Research.php
+++ Modules/RMS/Entities/Research.php
@@ -2,6 +2,7 @@
 
 namespace Modules\RMS\Entities;
 
+use App\Entities\Organization\Organization;
 use App\Entities\User;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,10 @@ class Research extends Model
     protected  $table = 'research';
     protected $fillable = ['title', 'submitted_by', 'status'];
 
+    public function organizations()
+    {
+        return $this->morphToMany(Organization::class, 'organizable');
+    }
 
     public function researchSubmittedByUser()
     {
