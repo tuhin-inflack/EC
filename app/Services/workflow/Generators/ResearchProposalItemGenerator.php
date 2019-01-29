8dc73b23c0fda9d0fbfcff45b5610b8f47515419
Md. Abutaleb <abutaleb@bs-23.net>
2019-01-29 12:38:14 +0600

    working on dynamic values integration in the workflow

--- app/Services/workflow/Generators/ResearchProposalItemGenerator.php
+++ app/Services/workflow/Generators/ResearchProposalItemGenerator.php
@@ -31,27 +31,36 @@ class ResearchProposalItemGenerator extends BaseDashboardItemGenerator
     }
 
 
-    public function generateItems() : DashboardItemSummary
+    public function generateItems(): DashboardItemSummary
     {
         $dashboardItemSummary = new DashboardItemSummary();
         $dashboardItems = array();
         $user = $this->userService->getLoggedInUser();
-        $designationId = 2;
+        $designationId = $this->userService->getDesignationId($user->username);
         $workflows = $this->workflowService->getWorkflowDetailsByUser($user->id, [$designationId]);
-        foreach ($workflows as $workflow) {
+        foreach ($workflows as $key => $workflow) {
             $dashboardItem = new DashboardItem();
             $workflowMaster = $workflow->workflowMaster;
+            dd($workflowMaster);
+
+            $descriptionData[$key] = [
+                'feature_name' => $workflowMaster->feature->name,
+                'rule_master_name' => $workflowMaster->ruleMaster->name,
+                'rule_details' => $workflowMaster->ruleMaster->rule,
+            ];
+            dd($descriptionData);
+
             $workflowConversation = $workflow->workflowConversations[0];
             $dashboardItem->setFeatureItemId($workflow->workflowMaster->feature->id);
             $dashboardItem->setFeatureName($workflowMaster->feature->name);
             $dashboardItem->setWorkFlowConversationId($workflowConversation->id);
             //TODO: set appropriate url
-            $dashboardItem->setCheckUrl('/edit/'.$workflowMaster->ref_table_id);
+            $dashboardItem->setCheckUrl('/edit/' . $workflowMaster->ref_table_id);
             $dashboardItem->setWorkFlowMasterId($workflowMaster->id);
             $dashboardItem->setWorkFlowMasterStatus($workflowMaster->status);
             $dashboardItem->setMessage($workflowConversation->message);
             //TODO: add dynamic items as array. Receive data from $workflowMaster reference id
-            $dashboardItem->setDynamicValues();
+//            $dashboardItem->setDynamicValues();
             array_push($dashboardItems, $dashboardItem);
         }
         $dashboardItemSummary->setDashboardItems($dashboardItems);
