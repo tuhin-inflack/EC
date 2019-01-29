270bc094b18f85b1ba19a4a675ab42ae7896d1ec
siam@inflack <Shadman Ahmed>
2019-01-29 16:36:08 +0600

    BE-416 Research detail page

--- Modules/RMS/Resources/views/research/show.blade.php
+++ Modules/RMS/Resources/views/research/show.blade.php
@@ -3,14 +3,16 @@
 
 @section('content')
     <section class="row">
-        <div class="col-7">
+        <div class="col-md-7">
             <div class="card">
                 <div class="card-header">
-                    <h4 class="card-title">Organizations</h4>
+                    <h4 class="card-title">@lang('organization.organization_list')</h4>
                     <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
                     <div class="heading-elements">
                         <ul class="list-inline mb-0">
-                            <li><a href="{{ route('organizations.create', $research->id) }}" class="btn btn-sm btn-primary"><i class="ft ft-plus"></i> Add Organization</a></li>
+                            <li><a href="{{ route('rms-organizations.create', $research->id) }}"
+                                   class="btn btn-sm btn-primary"><i
+                                            class="ft ft-plus"></i> @lang('organization.add_organization')</a></li>
                             <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
                             <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
                             <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
@@ -20,28 +22,22 @@
                 <div class="card-content">
                     <div class="card-body">
                         <div class="table-responsive">
-                            <table class="table table-striped table-bordered">
+                            <table class="table table-striped table-bordered organization-table">
                                 <thead>
                                 <tr>
-                                    <th>Organization</th>
-                                    <th>Attribute</th>
+                                    <th>@lang('labels.serial')</th>
+                                    <th>@lang('organization.organization')</th>
+                                    <th>@lang('organization.attribute')</th>
                                 </tr>
                                 </thead>
                                 <tbody>
-                                {{--@foreach($research->organizations as $organization)
-                                <tr>
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{ $loop->iteration }}</td>
-                                </tr>
-                                @endforeach--}}
-                                <tr>
-                                    <td>Inflack Limited</td>
-                                    <td>2</td>
-                                </tr>
-                                <tr>
-                                    <td>Brain Station 23</td>
-                                    <td>3</td>
-                                </tr>
+                                @foreach($research->organizations as $organization)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td><a href="#">{{ $organization->name }}</a></td>
+                                        <td>{{ $organization->attributes->count() }}</td>
+                                    </tr>
+                                @endforeach
                                 </tbody>
                             </table>
                         </div>
@@ -88,4 +84,14 @@
         </div>
     </section>
 @endsection
+
+@push('page-js')
+    <script>
+        $(document).ready(function () {
+            $('.organization-table').DataTable({
+                "pageLength": 5
+            })
+        });
+    </script>
+@endpush
 
