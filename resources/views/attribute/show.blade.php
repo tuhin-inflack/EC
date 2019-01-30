515a394c8f7b1006370072d55a7fc76b1e9fad66
siam@inflack <Shadman Ahmed>
2019-01-30 18:28:16 +0600

    BE-404 Oraganization Details Page
    - Update Attribute Details page

--- resources/views/attribute/show.blade.php
+++ resources/views/attribute/show.blade.php
@@ -9,19 +9,30 @@
                     <div class="card-header">
                         <a class="heading-elements-toggle"><i class="la la-ellipsis-h font-medium-3"></i></a>
                         <div class="heading-elements">
+                            <ul class="list-inline mb-0">
+                                <li><a href="{{ route($module . '-attribute-values.create', $attribute->id) }}"
+                                       class="btn btn-sm btn-primary"><i
+                                                class="ft ft-plus"></i> @lang('attribute.enter_value')</a></li>
+                                <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                                <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                                <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                            </ul>
                         </div>
                     </div>
                     <div class="card-content collapse show">
                         <div class="card-body card-dashboard">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <h4>@lang('attribute.organization'): {{ $organization->name }}</h4>
-                                </div>
-                                <div class="col-md-4">
-                                    <h4>@lang('attribute.attribute'): {{ $attribute->name }}</h4>
-                                </div>
-                                <div class="col-md-4">
-                                    <h4>@lang('attribute.unit'): {{ $attribute->unit }}</h4>
+                            <br>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <h4>@lang('attribute.organization'): {{ $organization->name }}</h4>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <h4>@lang('attribute.attribute'): {{ $attribute->name }}</h4>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <h4>@lang('attribute.unit'): {{ $attribute->unit }}</h4>
+                                    </div>
                                 </div>
                             </div>
                             <hr>
@@ -44,7 +55,9 @@
                                             <td>{{ \Carbon\Carbon::parse($value->date)->format('d/m/Y') }}</td>
                                             <td>{{ $value->planned_value }}</td>
                                             <td>{{ $value->achieved_value }}</td>
-                                            <td class="text-center"><a href="{{ route($module . '-attribute-values.edit', [$attribute->id, $value->id]) }}" class="btn btn-sm btn-info"><i class="ft ft-edit"></i></a></td>
+                                            <td class="text-center"><a
+                                                        href="{{ route($module . '-attribute-values.edit', [$attribute->id, $value->id]) }}"
+                                                        class="btn btn-sm btn-info"><i class="ft ft-edit"></i></a></td>
                                         </tr>
                                     @endforeach
                                     </tbody>
