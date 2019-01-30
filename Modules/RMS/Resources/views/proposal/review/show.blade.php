762c345bc6f58f9f072e1feda475b7d40e6eb1b8
Md. Abutaleb <abutaleb@bs-23.net>
2019-01-30 20:59:20 +0600

    button name rename to send back

--- Modules/RMS/Resources/views/proposal/review/show.blade.php
+++ Modules/RMS/Resources/views/proposal/review/show.blade.php
@@ -88,9 +88,9 @@
                                     {!! Form::hidden('item_id', $researchProposalSubmissionId) !!}
                                     {{--                                    {!! Form::button(' <i class="ft-skip-back"></i> Back', ['type' => 'submit', 'class' => 'btn btn-warning mr-1', 'name' => 'type', 'value' => 'publish'] ) !!}--}}
                                     <a class="btn btn-warning mr-1" role="button" href="{{ route('rms.index') }}">
-                                        <i class="ft-skip-back"></i> Back</a>
+                                        <i class="ft-x"></i> Cancel</a>
                                     {!! Form::button(' <i class="ft-check"></i> Approved', ['type' => 'submit', 'class' => 'btn btn-success mr-1', 'name' => 'status', 'value' => 'APPROVED'] ) !!}
-                                    {!! Form::button('  <i class="ft-x"></i> Reject', ['type' => 'submit', 'class' => 'btn btn-danger mr-1', 'name' => 'status', 'value' => 'REJECTED'] ) !!}
+                                    {!! Form::button('  <i class="ft-skip-back"></i> Send Back', ['type' => 'submit', 'class' => 'btn btn-danger mr-1', 'name' => 'status', 'value' => 'REJECTED'] ) !!}
 
                                     {!! Form::close() !!}
                                 </div>
