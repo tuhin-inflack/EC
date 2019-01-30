785de9513e3a6004307872de4cab8824a8606491
masattar-bs <abdus.sattar@bs-23.net>
2019-01-30 16:18:37 +0600

    Commit before pull, some changes in PMS workflow service

--- app/Services/workflow/Generators/ProjectProposalItemGenerator.php
+++ app/Services/workflow/Generators/ProjectProposalItemGenerator.php
@@ -11,6 +11,8 @@ namespace App\Services\workflow\Generators;
 
 use App\Models\DashboardItem;
 use App\Models\DashboardItemSummary;
+use App\Repositories\workflow\FeatureRepository;
+use App\Repositories\workflow\WorkflowConversationRepository;
 use App\Services\UserService;
 use App\Services\workflow\WorkflowService;
 use Modules\PMS\Services\ProjectProposalService;
@@ -20,11 +22,20 @@ class ProjectProposalItemGenerator extends BaseDashboardItemGenerator
     private $workflowService;
     private $userService;
     private $projectProposalService;
+    private $featureRepository;
+    private $flowConversationService;
 
-    public function __construct(WorkflowService $workflowService, UserService $userService, ProjectProposalService $projectProposalService)
+
+    public function __construct(WorkflowService $workflowService,
+                                UserService $userService,
+                                ProjectProposalService $projectProposalService,
+                                FeatureRepository $featureRepository,
+                                WorkflowConversationRepository $workflowConversationRepository)
     {
         $this->workflowService = $workflowService;
         $this->userService = $userService;
+        $this->projectProposalService = $projectProposalService;
+        $this->featureRepository = $projectProposalService;
         $this->projectProposalService = $projectProposalService;
     }
 
@@ -34,19 +45,17 @@ class ProjectProposalItemGenerator extends BaseDashboardItemGenerator
         $dashboardItems = array();
         $user = $this->userService->getLoggedInUser();
         $designationId = $this->userService->getDesignationId($user->username);
-        $workflows = $this->workflowService->getWorkflowDetailsByUser($user->id, [$designationId]);
-
+        $feature = $this->featureRepository->findOneBy(['name' => config('constants.project_proposal_feature_name')]);
+        $workflows = $this->workflowService->getWorkflowDetailsByUserAndFeature($user->id, [$designationId], $feature->id);
         foreach ($workflows as $key => $workflow) {
             $dashboardItem = new DashboardItem();
             $workflowMaster = $workflow->workflowMaster;
+            $proposal = $this->projectProposalService->findOne($workflowMaster->ref_table_id);
 
-            //dd($workflowMaster);
             $projectData = [
-                'feature_name' => $workflowMaster->feature->name,
-                'rule_master_name' => $workflowMaster->ruleMaster->name,
-                'rule_details' => $workflowMaster->ruleMaster->rule,
-                'project_title' => $workflowMaster->projectProposalSubmission->title,
-                'requested_by' => $workflowMaster->projectProposalSubmission->proposalSubmittedBy->name,
+                'feature_name' => $feature->name,
+                'project_title' => $proposal->title,
+                'requested_by' => $proposal->proposalSubmittedBy->name,
             ];
 
             $workflowConversation = $workflow->workflowConversations[0];
@@ -75,6 +84,36 @@ class ProjectProposalItemGenerator extends BaseDashboardItemGenerator
 
     public function generateRejectedItems(): DashboardItemSummary
     {
-        // TODO: Implement generateRejectedItems() method.
+        $dashboardItemSummary = new DashboardItemSummary();
+        $dashboardItems = array();
+        $user = $this->userService->getLoggedInUser();
+        $feature = $this->featureRepository->findOneBy(['name' => config('constants.research_proposal_feature_name')]);
+        $workflows = $this->workflowService->getRejectedItems($user->id, $feature->id);
+        foreach ($workflows as $key => $workflowMaster) {
+            $dashboardItem = new DashboardItem();
+            $researchData = [
+                'proposal_title' => $workflowMaster->researchProposalSubmission->title,
+                'research_title' => $workflowMaster->researchProposalSubmission->requester->title,
+                'remarks' => $workflowMaster->researchProposalSubmission->requester->remarks,
+            ];
+
+            $workflowConversation = $this->flowConversationService->getActiveConversationByWorkFlow($workflowMaster->id);
+            $dashboardItem->setFeatureItemId($feature->id);
+            $dashboardItem->setFeatureName($feature->name);
+            $dashboardItem->setWorkFlowConversationId($workflowConversation->id);
+            //TODO: set appropriate url (done)
+            $dashboardItem->setCheckUrl(
+                '/rms/research-proposal-submission/review/' . $workflowMaster->ref_table_id .
+                '/' . $feature->name . '/' . $workflowMaster->id . '/' . $workflowConversation->id);
+            $dashboardItem->setWorkFlowMasterId($workflowMaster->id);
+            $dashboardItem->setWorkFlowMasterStatus($workflowMaster->status);
+            $dashboardItem->setMessage($workflowConversation->message);
+            //TODO: add dynamic items as array. Receive data from $workflowMaster reference id (done)
+            $dashboardItem->setDynamicValues($researchData);
+            array_push($dashboardItems, $dashboardItem);
+        }
+
+        $dashboardItemSummary->setDashboardItems($dashboardItems);
+        return $dashboardItemSummary;
     }
 }
