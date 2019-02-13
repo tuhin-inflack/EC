a4c8769fde27a2b0b81289ed45b9bcd2b1d0294e
tanvirinflack <tanvir@inflack.com>
2019-02-13 13:15:59 +0600

    Added new Localization for Division

---
+++ resources/lang/bn/division.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'barishal' => 'বরিশাল',
+    'chittagong' => 'চট্টগ্রাম',
+    'dhaka' => 'ঢাকা',
+    'khulna' => 'খুলনা',
+    'mymensingh' => 'ময়মনসিংহ',
+    'rajshahi' => 'রাজশাহী',
+    'sylhet' => 'সিলেট',
+];
---
+++ resources/lang/en/division.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'barishal' => 'Barishal',
+    'chittagong' => 'Chittagong',
+    'dhaka' => 'Dhaka',
+    'khulna' => 'Khulna',
+    'mymensingh' => 'Mymensingh',
+    'rajshahi' => 'Rajshahi',
+    'sylhet' => 'Sylhet',
+];
