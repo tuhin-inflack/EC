688880f21c4654ce3a55886fc9a35b33735f1f9a
Md. Jahangir Alam <jahangir.alam@bs-23.com>
2019-02-13 10:42:23 +0600

    add no notification image

--- resources/views/notification/index.blade.php
+++ resources/views/notification/index.blade.php
@@ -3,43 +3,43 @@
 
 @section('content')
     <section id="notification-list">
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <div class="row">
-                            <div class="col-md-10"><h4 class="card-title">{{trans('labels.notification')}}</h4></div>
-                            <div class="col-md-2 text-right"><a href="{{route('notification.clear')}}">{{trans('labels.clear_all')}}</a>
-                            </div>
+        <div class="container">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-md-10"><h4 class="card-title">{{trans('labels.notification')}}</h4>
+                        </div>
+                        <div class="col-md-2 text-right"><a
+                                href="{{route('notification.clear')}}">{{trans('labels.clear_all')}}</a>
                         </div>
                     </div>
-                    <div class="card-content collapse show">
-                        <div class="card-body card-dashboard">
-                            <table class="table table-striped alt-pagination">
-                                <tbody>
-                                @if($notifications->count() == 0)
+                </div>
+                <div class="card-content collapse show">
+                    <div class="card-body card-dashboard">
+                        <table class="table table-striped alt-pagination">
+                            <tbody>
+                            @if($notifications->count() == 0)
+                                <tr>
+                                    <td>
+                                        {{trans('labels.no_notification')}}
+                                    </td>
+                                </tr>
+                            @else
+                                @foreach($notifications as $notification)
                                     <tr>
                                         <td>
-                                            {{trans('labels.no_notification')}}
+                                            <a href="{{$notification->item_url}}">{{$notification->message}}</a><br/>
+                                            <small>
+                                                <time class="media-meta text-muted"
+                                                      datetime="{{$notification->created_at}}">{{$notification->created_at}}</time>
+                                            </small>
                                         </td>
                                     </tr>
-                                @else
-                                    @foreach($notifications as $notification)
-                                        <tr>
-                                            <td>
-                                                <a href="{{$notification->item_url}}">{{$notification->message}}</a><br/>
-                                                <small>
-                                                    <time class="media-meta text-muted"
-                                                          datetime="{{$notification->created_at}}">{{$notification->created_at}}</time>
-                                                </small>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                @endif
-                                </tbody>
-                            </table>
-                            {{$notifications->links()}}
-                        </div>
+                                @endforeach
+                            @endif
+                            </tbody>
+                        </table>
+                        {{$notifications->links()}}
                     </div>
                 </div>
             </div>
@@ -47,7 +47,6 @@
     </section>
 @endsection
 @push('page-js')
-<script type="text/javascript">
-    $('body').removeClass('vertical-menu 2-columns menu-expanded');
-</script>
+    <script type="text/javascript">
+    </script>
 @endpush
