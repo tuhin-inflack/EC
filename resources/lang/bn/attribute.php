083546105bbb5643c0f4c2eb5da7d6c8d7f7abea
Shadman Ahmed <siam@inflack.com>
2019-02-28 13:36:07 +0600

    BE-552 Deposit details page

--- resources/lang/bn/attribute.php
+++ resources/lang/bn/attribute.php
@@ -29,4 +29,7 @@ return [
     'graph' => 'লেখচিত্র',
     'attribute_tabular_view' => 'বৈশিষ্ট্য ট্যাবুলার ভিউ',
     'duplicate_value_entry' => 'এই মাসে ভ্যালু  দেয়া আছে, নতুন ইনপুট সম্বব নয়',
+    'initial_balance' => 'প্রাথমিক ভারসাম্য',
+    'current_balance' => 'বর্তমান হিসাব',
+    'make_transaction' => 'লেনদেন করুন',
 ];
--- resources/lang/en/attribute.php
+++ resources/lang/en/attribute.php
@@ -29,4 +29,7 @@ return [
     'graph' => 'Graph',
     'attribute_tabular_view' => 'Attribute Tabular View',
     'duplicate_value_entry' => 'Attribute Value Already Added for this Month',
+    'initial_balance' => 'Initial Balance',
+    'current_balance' => 'Current Balance',
+    'make_transaction' => 'Make Transaction',
 ];
