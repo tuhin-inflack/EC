4c0aa9c57e8634da58a44401c24063849ff23be6
Md. Jahangir Alam <jahangir.alam@bs-23.com>
2019-03-03 11:50:15 +0600

    update workflow rule table

---
+++ database/migrations/2019_03_03_110040_add_get_back_status_btn_label_back_to_rule_workflow_rule_details_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddGetBackStatusBtnLabelBackToRuleWorkflowRuleDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('workflow_rule_details', function (Blueprint $table) {
+            $table->string('get_back_status')->default('PREVIOUS');//INITIAL, PREVIOUS, NONE, SELECTION,
+            $table->string('back_to_rule')->nullable();
+            $table->string('proceed_to_status')->default('NEXT'); //NEXT, SELECTION
+            $table->string('proceed_to_rule')->nullable();
+            $table->string('flow_type')->default('APPROVAL'); //APPROVAL and REVIEW
+            $table->string('is_optional')->default(false);
+            $table->string('back_btn_label')->default('Send Back');
+            $table->string('proceed_btn_label')->default('Approve');
+            $table->string('reject_btn_label')->default('Reject');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('workflow_rule_details', function (Blueprint $table) {
+            //
+        });
+    }
+}
