791b0ce6e736a2456514fd15d52cc5d66c15aa70
Md. Abu Taleb <abutaleb@bs-23.net>
2019-03-04 11:57:47 +0600

    ShareConversation and ShareRule model created

---
+++ app/Entities/Sharing/ShareConversation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Entities\Sharing;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ShareConversation extends Model
+{
+    protected $table = 'share_conversations';
+}
---
+++ app/Sharing/ShareConversation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Sharing;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ShareConversation extends Model
+{
+    //
+}
