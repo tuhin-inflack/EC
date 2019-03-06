132efcca7143d5d3633941389a1529753c128b51
Tuhin <tuhini@inflack.com>
2019-03-06 18:08:34 +0600

    BE-504: Add a separate training under a project 	- Option to associate members with the training

---
+++ Modules/PMS/Repositories/ProjectTrainingMemberRepository.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artisan33
+ * Date: 3/6/19
+ * Time: 4:12 PM
+ */
---
+++ Modules/PMS/Resources/views/project/training/show.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artisan33
+ * Date: 3/5/19
+ * Time: 4:44 PM
+ */
