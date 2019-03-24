1628808f7ba56a341f8d073a3569ae0d706e9486
Tuhin <tuhini@inflack.com>
2019-03-24 13:03:58 +0600

    Migration rollback issue solve

--- Modules/TMS/Resources/views/public/training-registration/partials/form/step-7.blade.php
+++ Modules/TMS/Resources/views/public/training-registration/partials/form/step-7.blade.php
@@ -27,24 +27,9 @@
                     <div class="form-group">
                         <div class="form-group">
                             <label for="emailAddress1" class="">@lang('tms::training.experience') :</label>
-                            {{ Form::select('academic_institute_id',['L' => 'Large', 'S' => 'Small'], NULL , ['class' => ' form-control instituteSelection',
+                            {{ Form::select('academic_institute_id',['Song' => 'Song', 'Acting' => 'Acting', 'Dancing' => 'Dancing', 'Speech' => 'Speech'], NULL , ['class' => ' form-control instituteSelection',
                                                 'placeholder' => trans('labels.select'),'data-validation-required-message'=>trans('labels.This field is required')]) }}
                         </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="emailAddress1" class="required">@lang('tms::training.experience') :</label>
-                        {{ Form::select('academic_institute_id',['L' => 'Large', 'S' => 'Small'], NULL , ['class' => ' form-control instituteSelection',
-                                            'placeholder' => trans('labels.select'),'data-validation-required-message'=>trans('labels.This field is required')]) }}
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="emailAddress1" class="required">@lang('tms::training.games') :</label>
-                        {{ Form::select('academic_institute_id',['L' => 'Large', 'S' => 'Small'], NULL , ['class' => ' form-control instituteSelection', 'data-validation-required-message'=>trans('labels.This field is required'), 'multiple' => 'multiple']) }}
                     </div>
                 </div>
             </div>
