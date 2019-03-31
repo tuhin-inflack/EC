e50332c8fff2fdcc36ff1a552dae1fed16fa2feb
tanvirinflack <tanvir@inflack.com>
2019-03-31 12:42:55 +0600

    BE-731 View publication related tasks to only research department

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Modules\PMS\Entities\Project;
 use Modules\RMS\Entities\Research;
@@ -20,6 +22,18 @@ class AppServiceProvider extends ServiceProvider
             'project' => Project::class,
             'research' => Research::class
         ]);
+
+        Blade::directive('designation', function ($expression) {
+            $haystack = explode(",", $expression);
+            $needle = Auth::user()->employee->designation->short_name;
+            $result = in_array($needle, $haystack) ? 1 : 0;
+
+            return "<?php if('{$result}') { ?>";
+        });
+
+        Blade::directive('enddesignation', function () {
+            return "<?php } ?>";
+        });
     }
 
     /**
