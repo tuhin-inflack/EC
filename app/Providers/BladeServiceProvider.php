e2693dbadcbf53e005c15ba613ff5e8f98471f1c
tanvirinflack <tanvir@inflack.com>
2019-04-01 17:41:06 +0600

    BE-731 Blade Directive Update

--- app/Providers/BladeServiceProvider.php
+++ app/Providers/BladeServiceProvider.php
@@ -23,7 +23,19 @@ class BladeServiceProvider extends ServiceProvider
             return "<?php if('{$result}') { ?>";
         });
 
-        Blade::directive('enddesignation', function () {
+        Blade::directive('elseif_designation', function ($expression) {
+            $haystack = explode(",", $expression);
+            $needle = Auth::user()->employee->designation->short_name;
+            $result = in_array($needle, $haystack) ? 1 : 0;
+
+            return "<?php } else if('{$result}') { ?>";
+        });
+
+        Blade::directive('else_designation', function () {
+            return "<?php } else { ?>";
+        });
+
+        Blade::directive('end_designation', function () {
             return "<?php } ?>";
         });
     }
