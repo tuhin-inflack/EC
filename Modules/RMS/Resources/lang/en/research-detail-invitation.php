7396ccfbb788d21a04d654ecd988f69e50d3b41f
yousha-inflack <yousha@inflack.com>
2019-04-05 08:05:09 +0600

    BE-695 | Detail Research Proposal invitation form.

---
+++ Modules/RMS/Resources/lang/en/research-detail-invitation.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+  'show' => [
+      'title' => 'Detail Research Proposal Information'
+  ],
+  'create' => [
+      'title' => 'Create Detail Request',
+      'form' => [
+          'create' => [
+              'title' => 'Detail Research Proposal Request Create Form'
+          ],
+          'update' => [
+              'title' => 'Detail Research Proposal Request Edit Form'
+          ]
+      ]
+  ],
+  'edit' => [
+      'title' => 'Edit Detail Request'
+  ]
+];
