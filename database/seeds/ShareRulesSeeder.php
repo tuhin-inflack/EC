d5ff7954195403471e618d5d215231ba0f077043
masattar-bs <abdus.sattar@bs-23.net>
2019-04-08 12:34:41 +0600

    PMS Workflow: changing seeders for implementing wf

--- database/seeds/ShareRulesSeeder.php
+++ database/seeds/ShareRulesSeeder.php
@@ -15,12 +15,19 @@ class ShareRulesSeeder extends Seeder
 
         \DB::table('share_rules')->insert([
                 1 => ['name' => 'Research Brief Sharing Rule - Director research will share with, ADR, JD, DDR, DG and ADG', 'is_group' => 1],
-                2 => ['name' => 'Project Sharing Rule', 'is_group' => 1],
-                3 => ['name' => 'ADR will share with DR and JD', 'is_group' => 1],
-                4 => ['name' => 'JD can share with DR, ADR, DDR', 'is_group' => 1],
-                5 => ['name' => 'DDR can share with JD and DR', 'is_group' => 1],
+                2 => ['name' => 'Project Sharing Rule, Director project with ADP, JD, DDR, DG and ADG', 'is_group' => 1],
+
+                3 => ['name' => 'ADR will share with DR and JDP', 'is_group' => 1],
+                4 => ['name' => 'JDP can share with DR, ADR, DDR', 'is_group' => 1],
+                5 => ['name' => 'DDR can share with JDP and DR', 'is_group' => 1],
                 6 => ['name' => 'DG can share with DR and ADG', 'is_group' => 1],
                 7 => ['name' => 'ADG can share with DG and DR', 'is_group' => 1],
+
+                8 => ['name' => 'ADP will share with PD and JDP', 'is_group' => 1],
+                9 => ['name' => 'JDP can share with PD, ADP, DDP', 'is_group' => 1],
+                10 => ['name' => 'DDP can share with JDP and PD', 'is_group' => 1],
+                11 => ['name' => 'ADG can share with PD and DG', 'is_group' => 1],
+                12 => ['name' => 'DG can share with ADG and PD', 'is_group' => 1],
             ]
         );
     }
