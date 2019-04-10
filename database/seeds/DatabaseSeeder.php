da14004b50b00908a9b6763799bd87624ea9ff2f
yousha-inflack <yousha@inflack.com>
2019-04-10 19:04:27 +0600

    Required database seeders are updated.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,30 +11,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(EconomyCodesTableSeeder::class);
         $this->call(EconomyHeadsTableSeeder::class);
+        $this->call(EconomyCodesTableSeeder::class);
+        $this->call(DepartmentsTableSeeder::class);
+        $this->call(DesignationsTableSeeder::class);
         $this->call(EmployeesTableSeeder::class);
         $this->call(FeaturesTableSeeder::class);
         $this->call(NotificationTypesTableSeeder::class);
-        $this->call(ProjectProposalFilesTableSeeder::class);
-        $this->call(ProjectProposalsTableSeeder::class);
-        $this->call(ProjectRequestAttachmentsTableSeeder::class);
-        $this->call(ProjectRequestForwardTableSeeder::class);
-        $this->call(ProjectRequestReceiversTableSeeder::class);
-        $this->call(ResearchRequestAttachmentsTableSeeder::class);
-        $this->call(ResearchRequestReceiversTableSeeder::class);
-        $this->call(ResearchRequestsTableSeeder::class);
-        $this->call(WorkflowRuleDetailsTableSeeder::class);
-        $this->call(WorkflowRuleMastersTableSeeder::class);
         $this->call(ShareRulesSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(WorkflowRuleMastersTableSeeder::class);
+        $this->call(WorkflowRuleDetailsTableSeeder::class);
         $this->call(ShareRulesDesignationSeeder::class);
-        $this->call(DistrictsTableSeeder::class);
         $this->call(DivisionsTableSeeder::class);
+        $this->call(DistrictsTableSeeder::class);
         $this->call(ThanasTableSeeder::class);
         $this->call(UnionsTableSeeder::class);
-        $this->call(DepartmentsTableSeeder::class);
-        $this->call(ShareRulesDesignationsTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
-        $this->call(DesignationsTableSeeder::class);
+        $this->call(RoomsTableSeeder::class);
+        $this->call(RoomTypesTableSeeder::class);
+        $this->call(HostelsTableSeeder::class);
     }
 }
