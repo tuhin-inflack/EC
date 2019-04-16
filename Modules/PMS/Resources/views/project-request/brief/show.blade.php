04317f64aa4c6a61810ecda7a557ab2d8f92b401
Tuhin <tuhini@inflack.com>
2019-04-16 20:07:59 +0600

    BE-988: Only project division can edit the brief project proposal

--- Modules/PMS/Resources/views/project-request/brief/show.blade.php
+++ Modules/PMS/Resources/views/project-request/brief/show.blade.php
@@ -67,9 +67,11 @@
 
                                 <div class="form-actions text-center">
                                     @if(\Carbon\Carbon::now()->lessThan($projectRequest->end_date))
-                                        <a href="{{ route('project-request.edit', $projectRequest->id) }}" class="btn btn-primary mr-1">
-                                            <i class="ft-plus white"></i> @lang('labels.edit')
-                                        </a>
+                                        @if(auth()->user()->employee->employeeDepartment->department_code == "PMS")
+                                            <a href="{{ route('project-request.edit', $projectRequest->id) }}" class="btn btn-primary mr-1">
+                                                <i class="ft-plus white"></i> @lang('labels.edit')
+                                            </a>
+                                        @endif
                                     @endif
                                     <a class="btn btn-warning mr-1" role="button" href="{{route('project-request.index')}}">
                                         <i class="ft-x"></i> @lang('labels.cancel')
