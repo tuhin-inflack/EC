5a0da495b7e8af027e666170641b9e56a318cf7f
tanvirinflack <tanvir@inflack.com>
2019-04-24 12:53:19 +0600

    BE-940 PMS:DPP Budget Calculation modification 	- Physical Contingency and Price Contingency calculation modified 	- Frontend Js modificaion

--- app/Services/DraftProposalBudget/DraftProposalBudgetService.php
+++ app/Services/DraftProposalBudget/DraftProposalBudgetService.php
@@ -51,20 +51,14 @@ class DraftProposalBudgetService
         ];
     }
 
-    public function getEconmonyCodeWiseSortedBudgets($budgetable){
+    public function getEconomyCodeWiseSortedBudgets($budgetable){
         return $budgetable->budgets()->orderBy('economy_code_id', 'asc')->get();
     }
 
-    /**
-     * @param $budgetable
-     * @return array
-     */
-    public function prepareBudgetView($budgetable)
+    public function getTotalExpenseOfRevenueAndCapital($budgetable)
     {
         $revenueExpense = 0;
         $capitalExpense = 0;
-        $physicalContingencyExpense = 0;
-        $priceContingencyExpense = 0;
 
         foreach ($budgetable->budgets as $budget)
         {
@@ -80,14 +74,28 @@ class DraftProposalBudgetService
 
         $totalExpense = $revenueExpense + $capitalExpense;
 
+        return compact('totalExpense', 'revenueExpense', 'capitalExpense');
+    }
+
+    /**
+     * @param $budgetable
+     * @return array
+     */
+    public function prepareDataForBudgetView($budgetable)
+    {
+        $physicalContingencyExpense = 0;
+        $priceContingencyExpense = 0;
+
+        list($totalExpense, $revenueExpense, $capitalExpense) = array_values($this->getTotalExpenseOfRevenueAndCapital($budgetable));
+
         foreach ($budgetable->budgets as $budget)
         {
             switch ($budget->section_type){
                 case 'physical_contingency':
-                    $physicalContingencyExpense = $budget->total_expense ? : ( $totalExpense * $budget->total_expense_percentage ) / 100 ;
+                    $physicalContingencyExpense = ( $totalExpense * $budget->total_expense_percentage ) / 100 ;
                     break;
                 case 'price_contingency':
-                    $priceContingencyExpense = $budget->total_expense ? : ( $totalExpense * $budget->total_expense_percentage ) / 100 ;
+                    $priceContingencyExpense = ( $totalExpense * $budget->total_expense_percentage ) / 100 ;
                     break;
             }
         }
