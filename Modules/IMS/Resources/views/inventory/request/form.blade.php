a55748a55fb4df9b4b0b47a3add7fee83b08ed7b
tanvirinflack <tanvir@inflack.com>
2019-05-22 14:47:40 +0600

    BE-1197 From Created with migration file update

--- Modules/IMS/Resources/views/inventory/request/form.blade.php
+++ Modules/IMS/Resources/views/inventory/request/form.blade.php
@@ -46,8 +46,8 @@
 <div class="row">
     <div class="col-md-2">
         <div class="form-group">
-            {!! Form::label('type', trans('ims::inventory.inventory_request_type'), ['class' => 'form-label required']) !!}
-            {!! Form::select('type',
+            {!! Form::label('request_type', trans('ims::inventory.inventory_request_type'), ['class' => 'form-label required']) !!}
+            {!! Form::select('request_type',
                 trans('ims::inventory.inventory_request_types'),
                 $page === 'create' ? null : $inventoryRequest->type,
                 [
@@ -56,14 +56,14 @@
             !!}
 
             <div class="help-block"></div>
-            @if ($errors->has('type'))
-                <span class="invalid-feedback">{{ $errors->first('type') }}</span>
+            @if ($errors->has('request_type'))
+                <span class="invalid-feedback">{{ $errors->first('request_type') }}</span>
             @endif
         </div>
     </div>
     <div class="col-md-5">
         <div class="form-group">
-            {!! Form::label('from_location_id', trans('ims::inventory.inventory_request_type'), ['class' => 'form-label required']) !!}
+            {!! Form::label('from_location_id', trans('ims::location.from_location'), ['class' => 'form-label required']) !!}
 
             {!! Form::select('from_location_id',
                 $fromLocations,
@@ -81,7 +81,7 @@
     </div>
     <div class="col-md-5">
         <div class="form-group">
-            {!! Form::label('to_location_id', trans('ims::inventory.inventory_request_type'), ['class' => 'form-label required']) !!}
+            {!! Form::label('to_location_id', trans('ims::location.to_location'), ['class' => 'form-label required']) !!}
 
             {!! Form::select('to_location_id',
                 $toLocations,
@@ -98,40 +98,78 @@
         </div>
     </div>
 </div>
+<h4 class="form-section"><i class="la la-tag"></i>@lang('ims::inventory.inventory_request')</h4>
 <div class="row">
     <div class="col-md-12">
         <table class="table table-bordered">
             <thead>
             <tr>
-                <th>@lang('ims::product.title')</th>
+                <th width="60%">@lang('ims::product.title')</th>
                 <th>@lang('labels.quantity')</th>
                 <th width="1%"><i class="la la-plus-circle text-info"></i></th>
             </tr>
             </thead>
             <tbody id="category-table">
-            @if($page === 'create')
                 <tr>
                     <td>
-{{--                        <input type="text" name="category_id[0]" class="form-control">--}}
-                        {!! Form::select('category_id',
-                            $itemCategories, null,
-                            [
-                                'class' => 'form-control select required'
-                            ])
-                        !!}
+                        {!! Form::select('category_id', $itemCategories, null, ['class' => 'form-control select required']) !!}
                     </td>
                     <td><input type="number" name="quantity[0]" min="0" class="form-control"></td>
                     <td><i class="la la-trash-o text-danger"></i></td>
                 </tr>
-            @elseif($page === 'edit')
-                @for($i = 0; $i <= 4; $i++)
-                    <tr>
-                        <td><input type="text" name="category_id[0]" class="form-control"></td>
-                        <td><input type="number" name="quantity[0]" min="0" class="form-control"></td>
-                        <td><i class="la la-trash-o text-danger"></i></td>
-                    </tr>
-                @endfor
-            @endif
+            </tbody>
+        </table>
+    </div>
+</div>
+<h4 class="form-section"><i class="la la-tag"></i> @lang('labels.new') @lang('ims::inventory.inventory_request')</h4>
+<div class="row">
+    <div class="col-md-12">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>@lang('ims::product.title') @lang('labels.name')</th>
+                    <th>@lang('ims::inventory.unit')</th>
+                    <th>@lang('ims::inventory.type')</th>
+                    <th>@lang('labels.quantity')</th>
+                    <th width="1%"><i class="la la-plus-circle text-info"></i></th>
+                </tr>
+            </thead>
+            <tbody id="category-table">
+            <tr>
+                <td><input type="text" name="name[]" class="form-control"></td>
+                <td><input type="text" name="unit[]" class="form-control"></td>
+                <td>
+                    <select name="type" class="form-control">
+                        <option value="1">@lang('ims::inventory.fixed_asset')</option>
+                        <option value="2">@lang('ims::inventory.stationery')</option>
+                    </select>
+                </td>
+                <td><input type="number" name="quantity[]" min="0" class="form-control"></td>
+                <td><i class="la la-trash-o text-danger"></i></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+<h4 class="form-section"><i class="la la-tag"></i>@lang('ims::inventory.inventory_request')</h4>
+<div class="row">
+    <div class="col-md-12">
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+                <th width="60%">@lang('ims::product.title')</th>
+                <th>@lang('labels.quantity')</th>
+                <th width="1%"><i class="la la-plus-circle text-info"></i></th>
+            </tr>
+            </thead>
+            <tbody id="category-table">
+            <tr>
+                <td>
+                    {!! Form::select('category_id', $itemCategories, null, ['class' => 'form-control select required']) !!}
+                </td>
+                <td><input type="number" name="quantity[0]" min="0" class="form-control"></td>
+                <td><i class="la la-trash-o text-danger"></i></td>
+            </tr>
             </tbody>
         </table>
     </div>
